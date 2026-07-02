8ac9bde149c509d07651047e304e2e370d3233ca
dariusbudi-max <dariusbudi@gmail.com>
2026-07-02 15:59:45 +0700

    Update driveService.js

--- src/services/driveService.js
+++ src/services/driveService.js
@@ -15,5 +15,20 @@ export const uploadToDrive = async (base64, filename) => {
         throw new Error(res?.message || "Upload gagal");
     }
 
-    return res.url;
+    return {
+        url: res.url,
+        fileId: res.fileId
+    };
+};
+
+export const deleteFromDrive = async (fileId) => {
+    if (!fileId) throw new Error("Drive File ID kosong.");
+
+    const res = await callAPI("DELETE_FROM_DRIVE", { fileId });
+
+    if (res?.status !== "success") {
+        throw new Error(res?.message || "Gagal menghapus file Drive.");
+    }
+
+    return true;
 };
