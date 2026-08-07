b9d32c66f291db929da65a175bfc307eefafc804
dariusbudi-max <dariusbudi@gmail.com>
2026-08-07 22:17:09 +0700

    Update pdfExport.js

--- src/exports/pdfExport.js
+++ src/exports/pdfExport.js
@@ -1,4 +1,6 @@
 import { downloadSPPPDF } from "./sppPdf.js";
 import { downloadBONPDF } from "./bonPdf.js";
+import { downloadBarcodePDF } from "./barcodePdf.js";
+import { downloadQrPDF } from "./qrCodePdf.js";
 
-export { downloadSPPPDF, downloadBONPDF };
+export { downloadSPPPDF, downloadBONPDF, downloadBarcodePDF, downloadQrPDF };
